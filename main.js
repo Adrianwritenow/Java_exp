68ff76ae874d94b702e4919d942d76f01c5bd310
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-07-05 10:32:54 -0400

    var vs let

---
+++ main.js
@@ -0,0 +1,29 @@
+
+let first_name = null; //null
+console.log(first_name);
+ if (first_name === null) {
+   console.log('Bruh im null')
+}
+
+let z =(5>9) //boolean
+console.log(z);
+  console.log("z is", typeof z);
+
+
+let newVar1 ="pig"//string
+console.log(newVar1);
+console.log(typeof newVar1);
+
+let newVar2; //undefined
+console.log(newVar2);
+console.log(typeof newVar2);
+
+
+var x = 9; //number
+console.log(x);
+console.log(typeof x);
+
+////mutate////
+var x = (3<6);
+console.log(x);
+console.log(typeof x);
